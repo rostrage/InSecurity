94d598b21f0a4a342198151f1a35678873f4d174
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-01-25 04:19:06 +0000

    Basic setup for serving static files.

---
+++ app.js
@@ -0,0 +1,12 @@
+var express=require('express'),
+app = express();
+
+app.use(express.static(__dirname+"/static"));
+app.use(app.router);
+
+app.get('/', function(req, res) {
+	res.redirect('/index.html');
+});
+
+app.listen(3000);
+
